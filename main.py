7d9fc3bb4f64648c52d57bfed6f2f1a22c0330ef
yagodanr <yagodanr@gmail.com>
2023-12-30 19:36:41 +0200

    Added SQLAlchemy based datebase users

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
-from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
+from db import schemas, crud, models
+from db.database import SessionLocal, engine
 
-class User(BaseModel):
-    id: int
-    username: str
-    email: str
-
-
-users: list[User] = list()
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/users")
-def get_all_users():
-    return users
+def get_all_users(db: Session=Depends(get_db)):
+    return crud.get_users(db)
 
 
 @app.post("/users")
-def create_user(user: User):
-    global users
-    users.append(user)
-    return user
+def create_user(user: schemas.UserCreate, db: Session=Depends(get_db)):
+
+    return crud.create_user(db, user)
 
 
 @app.get("/users/{user_id}")
-def get_user(user_id: int):
-    for us in users:
-        if us.id == user_id:
-            return us
-
-    return None
+def get_user(user_id: int, db: Session=Depends(get_db)):
+    return crud.get_user_by_id(db, user_id)
